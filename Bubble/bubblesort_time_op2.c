07afddf625605a3e76b69acd89105c862675937b
SAC-213 <ascencioncruzsaul@gmail.com>
2026-03-11 08:13:45 -0600

    Update bubblesort_time_op2.c

--- Bubble/bubblesort_time_op2.c
+++ Bubble/bubblesort_time_op2.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include "tiempo.h"
 
 //**************************************************
 //Libreria necesaria para medir rendimiento en Linux
 #include "tiempo.h"
 //**************************************************
-void bubbleop2(int A[], int n);
+
+void bubble (int A[],int n);
+
 int main(int argc, char *argv[])
 {
 //********************************************************
@@ -31,10 +32,10 @@ int main(int argc, char *argv[])
 //Iniciar el conteo del tiempo para las evaluaciones de rendimiento
 uswtime(&utime0, &stime0, &wtime0);
 //******************************************************************
-    bubbleop2(A,n);
+    bubble(A,n);
 //******************************************************************	
 //Evaluar los tiempos de ejecución 
-//uswtime(&utime1, &stime1, &wtime1);
+uswtime(&utime1, &stime1, &wtime1);
 //******************************************************************
     free(A);
 //***************************************************************************************************	
@@ -54,23 +55,19 @@ uswtime(&utime0, &stime0, &wtime0);
 	printf("sys (Tiempo en acciónes de E/S)  %.10e s\n",  stime1 - stime0);
 	printf("CPU/Wall   %.10f %% \n",100.0 * (utime1 - utime0 + stime1 - stime0) / (wtime1 - wtime0));
 	printf("\n");
-//***************************************************************************************************	
+//***************************************************************************************************
 return 0;
 }
 
-void bubbleop2 (int A[],int n){
+void bubble (int A[],int n){
     int i,j,aux;
-    int cambios = 1;
-    while(i<=n-2 && cambios != 0){
-        cambios = 0;
-        for(j=0;j<=(n-2)-i;j++){
+    for(i=0;i<=n-2;i++){
+        for(j=0;j<=n-2;j++){
             if(A[j] > A[j+1]){
                 aux = A[j];
                 A[j] = A[j+1];
                 A[j+1] = aux;
-                cambios = 1;
             }
         }
-        i=i+1;
     }
 }
